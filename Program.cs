be25dc9b3b9ba5d05b5a3fe24601bc777a4ca7bd
Rogue Ops <1724215+rogueops@users.noreply.github.com>
2020-09-27 19:48:09 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Hello World! Program
+// By Jason Plimpton
+// For Class CIS-162AD
+// 09/06/2020
+
+namespace Program
+{
+    class Print
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!\r\n");
+            System.Console.WriteLine("This is Jason Plimpton's first C# app!\r\n");
+            System.Console.WriteLine("Press enter to close...");
+            System.Console.ReadLine();
+        }
+    }
+}
